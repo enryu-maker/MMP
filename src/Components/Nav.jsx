7df0bf683d449422a01e6512b40a33826586f0ba
tejastejale <tejastejale13@gmail.com>
2024-06-29 13:01:28 +0530

    nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,8 +5,8 @@ import logo from "../images/logo2.png";
 export function Nav() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const handleDropdownToggle = (isOpen) => {
-    setIsDropdownOpen(isOpen);
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen((prev) => !prev);
   };
 
   return (
@@ -17,25 +17,31 @@ export function Nav() {
         className="shadow-md p-5 text-black top-0 sticky z-50 bg-white"
       >
         <div
-          className={`flex md:flex-row  justify-between items-center font-Poppins w-[88%] self-center`}
+          className={`flex md:flex-row ${
+            isDropdownOpen ? "flex-col gap-3" : "flex-row"
+          } justify-between items-center font-Poppins w-[88%] self-center`}
         >
           <Navbar.Brand href="https://flowbite-react.com">
             <span className="self-center whitespace-nowrap text-lg font-semibold dark:text-white">
               <img src={logo} className="h-10" />
             </span>
           </Navbar.Brand>
-          <Navbar.Toggle
-            onChange={handleDropdownToggle}
-            onClick={handleDropdownToggle}
-          />
+          <div onClick={handleDropdownToggle}>
+            <Navbar.Toggle />
+          </div>
           <Navbar.Collapse>
             <Navbar.Link
               href="#"
-              className="text-lg text-black hover:text-green-500"
+              className="text-lg text-black hover:text-green-500  text-center md:w-fit w-full items-center align-middle h-full"
             >
-              <p className="text-lg hover:text-green-500 text-black">Home</p>
+              <p className="text-lg hover:text-green-500 text-black items-center align-middle h-full">
+                Home
+              </p>
             </Navbar.Link>
-            <Navbar.Link href="#/about" className="text-lg text-black">
+            <Navbar.Link
+              href="#/about"
+              className="text-lg text-black text-center md:w-fit w-full items-center align-middle h-full"
+            >
               <p className="text-lg hover:text-green-500 text-black">
                 About Us
               </p>
@@ -43,7 +49,7 @@ export function Nav() {
             <Dropdown
               arrowIcon={true}
               inline={true}
-              className="text-center md:w-fit w-full items-center"
+              className="text-center md:w-fit w-full items-center align-middle h-full"
               // onOpenChange={handleDropdownToggle}
               label={
                 <p className="text-lg md:py-0 py-2 text-center w-full items-center hover:text-green-500 text-black cursor-pointer">
@@ -70,7 +76,7 @@ export function Nav() {
             <Dropdown
               arrowIcon={true}
               inline={true}
-              className="text-center md:w-fit w-full items-center"
+              className="text-center md:w-fit w-full items-center align-middle h-full"
               // onOpenChange={handleDropdownToggle}
               label={
                 <p className="text-lg md:py-0 py-2 text-center w-full items-center hover:text-green-500 text-black cursor-pointer">
@@ -97,7 +103,7 @@ export function Nav() {
             <Dropdown
               arrowIcon={true}
               inline={true}
-              className="text-center md:w-fit w-full items-center"
+              className="text-center md:w-fit w-full items-center align-middle h-full"
               // onOpenChange={handleDropdownToggle}
               label={
                 <p className="text-lg md:py-0 py-2 text-center w-full items-center hover:text-green-500 text-black cursor-pointer">
@@ -109,8 +115,13 @@ export function Nav() {
                 All Projects
               </Dropdown.Item>
             </Dropdown>
-            <Navbar.Link href="#/contact-us" className="text-lg text-black">
-              <p className="text-lg hover:text-green-500 text-black">Contact</p>
+            <Navbar.Link
+              href="#/contact-us"
+              className="text-lg text-black items-center align-middle h-full"
+            >
+              <p className="text-lg hover:text-green-500 text-black text-center md:w-fit w-full items-center align-middle h-full">
+                Contact
+              </p>
             </Navbar.Link>
           </Navbar.Collapse>
         </div>
